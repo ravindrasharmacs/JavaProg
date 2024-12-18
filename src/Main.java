6740a5ec46c63ca96d4ae48f47c53406b8170368
ravindrasharmacs <ravindra.sharmacs@gmail.com>
2024-12-18 11:58:11 +0530

    java_002: merge 2 sorted arrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {6, 7, 8, 9, 10};
+
+        int[] mergedArray = mergeArrays(arr1, arr2);
+        for (int i = 0; i < mergedArray.length; i++) {
+            System.out.print(mergedArray[i] + " ");
+        }
+    }
+
+    private static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        return result;
     }
 }
